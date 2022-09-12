779e3059327a397044b375b0185c89712cd0ab40
Yushkov Vitaliy <98348805+Yushkov88@users.noreply.github.com>
2022-09-12 07:32:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,31 @@ import { renderGallery } from './js/renderGallery';
 const searchForm = document.getElementById('search-form');
 const btnLoadMore = document.querySelector('.load-more');
 const galleryWrapper = document.querySelector('.gallery');
+let page = 1;
+let searchQuery = '';
+
+
+const showLastPageMessage = ({ total, totalHits }) => {
+  if (total === totalHits) {
+    btnLoadMore.classList.add('hidden');
+    Notify.warning("We're sorry, but you've reached the end of search results.");
+  }
+}
+
+
+btnLoadMore.addEventListener('click', async () => {
+  const { data } = await getImgs(searchQuery, ++page);
+  galleryWrapper.insertAdjacentHTML('beforeend', renderGallery(data.hits));
+  showLastPageMessage(data);
+});
 
 searchForm.addEventListener('submit', async event => {
   event.preventDefault();
 
-  const { data } = await getImgs(event.target.elements.searchQuery.value);
+  searchQuery = event.target.elements.searchQuery.value;
+  page = 1;
+
+  const { data } = await getImgs(searchQuery, page);
   galleryWrapper.innerHTML = '';
 
   if (!data.total) {
@@ -24,4 +44,7 @@ searchForm.addEventListener('submit', async event => {
   galleryWrapper.innerHTML = renderGallery(data.hits);
 
   btnLoadMore.classList.remove('hidden');
+
+  showLastPageMessage(data);
 });
+
